9846505426cc2106fd398eb5d4dca6d912f4c32c
TAEHO SIM <taehosim@TAEHOui-MacBookAir.local>
2025-03-20 16:53:17 +0900

    이자계산식 수정, Account 다형성 구축, 입력스트림 예외처리, 신용계좌, 일반계좌 추가

--- VC_C++/OldBookCode/main.cpp
+++ VC_C++/OldBookCode/main.cpp
@@ -3,8 +3,67 @@
 #include <string>
 #include <limits>
 #include <cctype>
+#include <memory>
 
 using namespace std;
+
+enum class BaseInterRate
+{
+    baseRate = 1
+};
+
+enum class CreditRate
+{
+    A = 7,
+    B = 4,
+    C = 2,
+};
+void cinClear()
+{
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+int MakeCreditAccountSelect()
+{
+    int select;
+
+    while (true)
+    {
+        cout << endl;
+        cout << "--------- Select Credit Rate Menu ---------" << endl;
+        cout << "1. Credit Rate A" << endl;
+        cout << "2. Credit Rate B" << endl;
+        cout << "3. Credit Rate C" << endl;
+        cout << "Select an option: ";
+
+        cin >> select;
+        if (cin.fail() || !(select < 4 && select > 0))
+        {
+            cout << "Illegal selection. Please try again" << endl;
+            cinClear();
+            continue;
+        }
+        else
+        {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            break;
+        }
+    }
+
+    switch (select)
+    {
+    case 1:
+        return static_cast<int>(CreditRate::A);
+    case 2:
+        return static_cast<int>(CreditRate::B);
+    case 3:
+        return static_cast<int>(CreditRate::C);
+    default:
+        cout << "Illegal selection. Please try again" << endl;
+    }
+    return 0;
+}
 
 class Account // 계좌에 관련된 클래스
 {
@@ -17,14 +76,15 @@ public:
     Account(int id, int init_Balance, const std::string &name)
         : accID(id), balance(init_Balance), cusName(name) {}
 
-    void display() const
+    virtual ~Account() {}
+    void Display() const
     {
         cout << "Account ID: " << accID << endl;
         cout << "Customer Name: " << cusName << endl;
         cout << "Balance: " << balance << endl;
     }
 
-    void deposit(int money)
+    virtual void deposit(int money)
     {
         if (money > 0)
             balance += money;
@@ -52,11 +112,45 @@ public:
         return balance;
     }
 };
+class NormalAccount : public Account
+{
+private:
+    int interRate;
+
+public:
+    NormalAccount(int id, int balance, const std::string &name, int rate)
+        : Account(id, balance, name), interRate(rate) {}
+
+    virtual void deposit(int in_money) override
+    {
+        //  Account::deposit(in_money);
+        int inter = (getBalance() + in_money) * interRate / 100;
+        Account::deposit(in_money + inter);
+    }
+    ~NormalAccount() {}
+};
+
+class CreditAccount : public NormalAccount
+{
+private:
+    int creditInterRate;
+
+public:
+    CreditAccount(int id, int balance, const std::string &name, int rate, int credit_rate)
+        : NormalAccount(id, balance, name, rate), creditInterRate(credit_rate) {}
+
+    virtual void deposit(int in_money) override
+    {
+        int inter = (getBalance() + in_money) * creditInterRate / 100;
+        NormalAccount::deposit(in_money + inter);
+    }
+    ~CreditAccount() {}
+};
 
 class BankingSystem
 {
 private:
-    std::vector<Account> accounts;
+    std::vector<std::unique_ptr<Account>> accounts;
 
 public:
     bool isValidName(const string &name)
@@ -100,7 +194,6 @@ public:
         int accID;
         while (true)
         {
-            cout << "Enter your desired ID number: ";
             if (cin >> accID) // int외 fail() else로 넘어감
             {
                 break;
@@ -108,8 +201,7 @@ public:
             else
             {
                 cout << "Invalid input. Please enter a valid numeric Account ID" << endl;
-                cin.clear();                                                   // cin 의 오류 초기화
-                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // 입력 버퍼 정리
+                cinClear();
             }
         }
         cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // 정상 입력 후 루프문 탈출시 남아있는 개행 문자 제거
@@ -118,32 +210,34 @@ public:
     int inputBalance()
     {
         int money;
-        cout << "Balance: ";
+
         while (!(cin >> money))
         {
             cout << "Invalid input. Please enter a valid numeric balance" << endl;
-            cin.clear();
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cinClear();
         }
         return money;
     }
     // TODO:std::Optional은 여기선 이질적인 느낌이라 일단 보류
     // 중복 체크 루프 함수
-    Account *findAccountByID(int &id)
+    Account *findAccountByID(int id)
     {
         for (auto &account : accounts)
         {
-            if (account.getID() == id)
-                return &account;
+            if (account->getID() == id)
+                return account.get();
         }
         return nullptr;
     }
-
     void makeAccount()
     {
         int id;
+        int balance;
+        string name;
+
         while (true)
         {
+            cout << "Enter your desired ID number: ";
             id = inputID();
             if (findAccountByID(id) != nullptr)
             {
@@ -153,50 +247,118 @@ public:
             break;
         }
 
-        string name = inputName();
-        int balance = inputBalance();
+        name = inputName();
+
+        cout << "Initial Deposit: ";
+        balance = inputBalance();
 
         // 생성시 어카운트 객체 추가
-        accounts.emplace_back(id, balance, name);
+        accounts.emplace_back(make_unique<Account>(id, balance, name));
         cout << "Account Creation Completed" << endl;
         cout << "ID: " << id << endl;
         cout << "Balance: " << balance << endl;
         cout << "Name: " << name << endl;
     }
+    void makeNormalAccount()
+    {
+        int id;
+        int balance;
+        string name;
+        int interRate = static_cast<int>(BaseInterRate::baseRate);
+        while (true)
+        {
+            cout << "Enter your desired ID number: ";
+            id = inputID();
+            if (findAccountByID(id) != nullptr)
+            {
+                cout << "Duplicate ID. Please Re-enter" << endl;
+                continue;
+            }
+            break;
+        }
 
+        name = inputName();
+        cout << "Initial Deposit: ";
+        balance = inputBalance();
+
+        // 생성시 어카운트 객체 추가
+        accounts.emplace_back(make_unique<NormalAccount>(id, balance, name, interRate));
+        cout << "Account Creation Completed" << endl;
+        cout << "ID: " << id << endl;
+        cout << "Balance: " << balance << endl;
+        cout << "Name: " << name << endl;
+    }
+    void makeCreditAccount()
+    {
+        int id;
+        int balance;
+        string name;
+        int interRate = static_cast<int>(BaseInterRate::baseRate);
+        int creditRate;
+
+        while (true)
+        {
+            cout << "Enter your desired ID number: ";
+            id = inputID();
+            if (findAccountByID(id) != nullptr)
+            {
+                cout << "Duplicate ID. Please Re-enter" << endl;
+                continue;
+            }
+            break;
+        }
+
+        name = inputName();
+        cout << "Initial Deposit: ";
+        balance = inputBalance();
+        creditRate = MakeCreditAccountSelect();
+
+        // 생성시 어카운트 객체 추가
+        accounts.emplace_back(make_unique<CreditAccount>(id, balance, name, interRate, creditRate));
+        cout << "Account Creation Completed" << endl;
+        cout << "ID: " << id << endl;
+        cout << "Balance: " << balance << endl;
+        cout << "Name: " << name << endl;
+        cout << "CreditRate: " << creditRate << endl;
+    }
     void depositMoney()
     {
-        int id = inputID();
-        int money = inputBalance();
 
-        Account* account = findAccountByID(id);
-        
-        
-        if(account)
+        int id;
+        int money;
+
+        cout << "Enter your desired ID number: ";
+        id = inputID();
+
+        Account *account = findAccountByID(id);
+
+        if (account)
         {
+            cout << "Enter Deposit: ";
+            money = inputBalance();
             account->deposit(money);
             cout << "Deposit Completed" << endl;
             cout << "Current Balance: " << account->getBalance() << endl;
         }
         else
         {
-            cout << "Account not found" <<endl;
+            cout << "Account not found" << endl;
         }
     }
 
     void withdrawMoney()
     {
         int id, money;
-        cout << "Enter your Account ID for withdrawal: ";
+        cout << "Enter your Account ID for Withdrawal: ";
         id = inputID();
-        cout << "Enter withdrawal amount: ";
-        money = inputBalance();
 
-        Account* account = findAccountByID(id);
-        
-        if(account)
+        Account *account = findAccountByID(id);
+
+        if (account)
         {
-            if(account -> withdraw(money))
+            cout << "Enter Withdrawal amount: ";
+            money = inputBalance();
+            if (account->withdraw(money))
             {
                 cout << "Withdrawal Completed" << endl;
             }
@@ -204,7 +366,7 @@ public:
             {
                 cout << "Insufficient funds" << endl;
             }
-            cout << "Current Balance: " << account -> getBalance() << endl;
+            cout << "Current Balance: " << account->getBalance() << endl;
         }
         else
         {
@@ -220,7 +382,7 @@ public:
         }
         for (const auto &account : accounts)
         {
-            account.display();
+            account->Display();
             cout << "--------------------------" << endl;
         }
     }
@@ -229,13 +391,54 @@ public:
 void showMenu()
 {
     cout << endl;
-    cout << "--------- Banking System Menㅕ ---------" << endl;
+    cout << "--------- Banking System Menu ---------" << endl;
     cout << "1. Make Account" << endl;
     cout << "2. Deposit Money" << endl;
     cout << "3. Withdraw Money" << endl;
     cout << "4. Inquire All Accounts" << endl;
     cout << "5. Exit" << endl;
     cout << "Select an option: ";
+}
+
+void MakeAccountSelect(BankingSystem &bankSystem)
+{
+    int select;
+
+    while (true)
+    {
+        cout << endl;
+        cout << "--------- Make Account Menu ---------" << endl;
+        cout << "1. Make Normal Account" << endl;
+        cout << "2. Make Credit Account" << endl;
+        cout << "Select an option: ";
+
+        cin >> select;
+        if (cin.fail() || !(select == 1 || select == 2))
+        {
+            cout << "Illegal selection. Please try again" << endl;
+            cinClear();
+            continue;
+        }
+        else
+        {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            break;
+        }
+    }
+
+    switch (select)
+    {
+    case 1:
+        // TODO: 보통계좌 계설
+        bankSystem.makeNormalAccount();
+        break;
+    case 2:
+        // TODO: 신용계좌 계설
+        bankSystem.makeCreditAccount();
+        break;
+    default:
+        cout << "Illegal selection. Please try again" << endl;
+    }
 }
 
 int main()
@@ -252,21 +455,26 @@ int main()
         switch (choice)
         {
         case 1:
-            bankSystem.makeAccount();
+            MakeAccountSelect(bankSystem);
+
             break;
         case 2:
             bankSystem.depositMoney();
+
             break;
         case 3:
             bankSystem.withdrawMoney();
+
             break;
         case 4:
             bankSystem.printAllAccounts();
+
             break;
         case 5:
             std::cout << "Exiting the program..." << endl;
             return 0;
         default:
+            cinClear();
             std::cout << "Illegal selection. Please try again." << endl;
         }
     }
