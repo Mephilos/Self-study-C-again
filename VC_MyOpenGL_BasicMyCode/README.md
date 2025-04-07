89cbf84deeb2e452d2fe4a75cc7781f374770451
TAEHO SIM <taehosim@TAEHOui-MacBookAir.local>
2025-04-08 00:07:02 +0900

    Planning to develop an OPENGL FPS mini game

---
+++ VC_MyOpenGL_BasicMyCode/README.md
@@ -0,0 +1,21 @@
+VC_MyOpenGL_BasicMyCode / 4. 8. 25
+
+OpenGL로 FPS 게임 엔진 기틀 만들기
+
+전체적인 구조 틀 계획
+
+----
+Camera // 카메라 컨트롤
+Input // 플레이어 컨트롤
+Gamehandler // 스코어, 게임씬, 전반적인 게임 시스템 관리
+Renderer // 렌더러  
+----
+Shader 
+----
+resources
+----
+main.cpp
+
+
+추후 세분화 예정 GameScenes, GameController, Score 등
+
---
+++ VC_MyOpenGL_BasicMyCode/build/CMakeFiles/main_OpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_OpenGL.dir/glad/src/glad.c.o"
+  "CMakeFiles/main_OpenGL.dir/glad/src/glad.c.o.d"
+  "CMakeFiles/main_OpenGL.dir/src/main.cpp.o"
+  "CMakeFiles/main_OpenGL.dir/src/main.cpp.o.d"
+  "main_OpenGL"
+  "main_OpenGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main_OpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
